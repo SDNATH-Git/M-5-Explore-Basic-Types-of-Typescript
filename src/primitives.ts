e8cbc924cc5b2753576449dfe3385e4f5a2914a1
SDNATH-Git <sd.nath1952@gmail.com>
2025-11-12 21:24:29 +0600

    added a type

--- src/primitives.ts
+++ src/primitives.ts
@@ -1,8 +1,8 @@
 // js -- ts
 
-let username = "SD";
+let name = "SD";
 // username= 2364;
-username = 'SD NATH';
+name = 'SD NATH';
 
 
 let ID:number = 1312;
@@ -11,4 +11,13 @@ ID = 555;
 
 let x = undefined;
 x= 12 ;
+
+
+let username: string = "Shuvo";
+let age: number = 22;
+let isStudent: boolean = true;
+let salary: number = 25000.5;
+let emptyValue: null = null;
+let notSet: undefined = undefined;
+let bigNumber: bigint = 987654321987654321n;
 
