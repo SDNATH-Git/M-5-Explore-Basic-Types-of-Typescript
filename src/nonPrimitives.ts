cac6d9450b3b2c48103fdd8366ebe9cd3034b5bb
SDNATH-Git <sd.nath1952@gmail.com>
2025-11-12 21:57:13 +0600

    non primitives data type

---
+++ src/nonPrimitives.ts
@@ -0,0 +1,10 @@
+// array, any , tuple, enum , void, null, undefined, never
+
+//array
+
+let fruits = ['mango','banana', 'apple',232];
+fruits.push(12);
+
+//tuple
+let person:[string,number,boolean] =['SD',22, true];
+person.push(2222);
