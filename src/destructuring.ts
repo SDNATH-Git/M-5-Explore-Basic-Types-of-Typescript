22bb96091cde784e0d6a98e4c460f67194105ca2
SDNATH-Git <sd.nath1952@gmail.com>
2025-11-13 02:14:02 +0600

    addded destruring objects

--- src/destructuring.ts
+++ src/destructuring.ts
@@ -5,3 +5,17 @@ let [a,b,c] = fruits;
 console.log(a);
 console.log(b);
 console.log(c);
+
+
+
+// Destructuring Objects
+const user ={
+    name: "SD NATH",
+    age : 22,
+    address : "ctg, Bangladesh",
+}
+
+const {name, age, address}= user;
+console.log(name);
+console.log(age);
+console.log(address);
