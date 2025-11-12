feb9f2761af662f1c6ba0baaceb2c8b5a781d8e4
SDNATH-Git <sd.nath1952@gmail.com>
2025-11-13 02:37:39 +0600

    type alias

---
+++ src/typeAlias.ts
@@ -0,0 +1,19 @@
+// Type Alias
+type user = {
+    name: string,
+    age: number,
+    isstudent : boolean,
+}
+
+let student1: user ={
+    name: "SD",
+    age: 23,
+    isstudent: true,
+}
+let student2: user ={
+    name: "Ajoy",
+    age: 23,
+    isstudent: true,
+}
+
+console.log(student1,student2);
