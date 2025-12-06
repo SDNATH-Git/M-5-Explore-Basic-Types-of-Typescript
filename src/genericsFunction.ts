dec90c333cf13f776afe8120ae17dc25b4e36806
SDNATH-Git <sd.nath1952@gmail.com>
2025-12-07 01:34:48 +0600

    addedd a fun

--- src/genericsFunction.ts
+++ src/genericsFunction.ts
@@ -1,3 +1,20 @@
 // Generics Function 
 // node src/genericsFunction.ts
 
+
+interface ApiResponse<T> {
+  status: number;
+  data: T;
+}
+
+function createResponse<T>(data: T): ApiResponse<T> {
+  return { status: 200, data };
+}
+
+const userRes = createResponse({ id: 1, name: "Shuvo" });
+const productRes = createResponse({ id: 11, title: "Laptop" });
+
+console.log(userRes);
+
+
+
