55e7e49d12928577196f59b398868f01b2799f0d
SDNATH-Git <sd.nath1952@gmail.com>
2025-12-08 23:31:14 +0600

    added constraint.ts

--- src/constraint.ts
+++ src/constraint.ts
@@ -1,8 +1,12 @@
 // Constraint 
+// node src/constraint.ts
 
 function printValue<T extends object>(value: T){
     console.log(value);
 
 }
 
-printValue({name: "SD", age:23});
+printValue({name: "SD", age:23, job: "Developer"});
+
+// printValue(10);                // ❌ error — object না
+
